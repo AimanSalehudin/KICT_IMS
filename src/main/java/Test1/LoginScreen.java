9addfcf19f2f793b6c111ba4e084716eb5e30f55
Nickopara <muhammad629859@gmail.com>
2025-05-25 15:50:24 +0800

    edit fileutil

--- src/main/java/Test1/LoginScreen.java
+++ src/main/java/Test1/LoginScreen.java
@@ -29,10 +29,12 @@ public class LoginScreen {
         Label passLabel = new Label("Password:");
         PasswordField passwordField = new PasswordField();
         Button loginBtn = new Button("Login");
+        Button exitBtn = new Button("Exit");
         Label message = new Label();
 
         // Styling
         loginBtn.setStyle("-fx-background-color: #2a9df4; -fx-text-fill: white; -fx-font-weight: bold;");
+        exitBtn.setStyle("-fx-background-color: #f44336; -fx-text-fill: white; -fx-font-weight: bold;");
         usernameField.setStyle("-fx-padding: 8px;");
         passwordField.setStyle("-fx-padding: 8px;");
         message.setStyle("-fx-text-fill: red;");
@@ -49,13 +51,16 @@ public class LoginScreen {
             }
         });
 
+        exitBtn.setOnAction(e -> stage.close() );
+
         GridPane grid = new GridPane();
         grid.setAlignment(Pos.CENTER);
         grid.setPadding(new Insets(20));
         grid.setVgap(15); grid.setHgap(15);
         grid.add(userLabel, 0, 1); grid.add(usernameField, 1, 1);
         grid.add(passLabel, 0, 2); grid.add(passwordField, 1, 2);
-        grid.add(loginBtn, 1, 3); grid.add(message, 1, 4);
+        grid.add(loginBtn, 1, 3); grid.add(exitBtn, 2, 3);
+        grid.add(message, 1, 4);
 
         VBox root = new VBox(15, logo, welcomeLabel, grid);
         root.setAlignment(Pos.TOP_CENTER);
